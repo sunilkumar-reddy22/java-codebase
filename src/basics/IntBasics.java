592ade50a93ba3ad64dbdf198a3a352ec2a115b6
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-01-04 22:36:00 +0530

    Add  user input basic programs

---
+++ src/basics/IntBasics.java
@@ -0,0 +1,7 @@
+package basics;
+
+public class IntBasics {
+    public static void main(String[] args){
+        System.out.println("Hello new ide");
+    }
+}
---
+++ src/basics/Rollno.java
@@ -0,0 +1,13 @@
+package basics;
+import java.util.Scanner;
+
+public class Rollno {
+    public static void main(String[] args){
+        System.out.println("Enter your Rollno");
+         Scanner sc = new Scanner(System.in);
+         int rollno = sc.nextInt();
+         System.out.println("Your roll number is " + rollno);
+
+
+    }
+}
