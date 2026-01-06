9d705fc679467e6d229587d14a8131bfbd65518c
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-01-06 22:08:35 +0530

    Add while loop prgram

---
+++ src/basics/While.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class While {
+    public static void main(String[] args) {
+        int count = 1;
+        while(count != 10) {
+            System.out.println(count);
+            count++;
+        }
+    }
+}
