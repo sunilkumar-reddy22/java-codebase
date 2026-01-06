3f166ee0377cad96391a8b53a00734f9a6589f3d
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-01-06 17:00:13 +0530

    Add typepromotion

---
+++ src/basics/TypePromotion.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class TypePromotion {
+    public static void main(String[] args) {
+        int x = 50000;
+        int n = 'A';
+        byte b = 100;
+        char c = 'a';
+        float f = 6453.23f;
+        double d = 7373298.2345;
+        double result = (x * n) + (b / c) - (f -d);
+        System.out.println(( x * n) + " " + (b / c) + " " + (f - d));
+
+
+    }
+}
