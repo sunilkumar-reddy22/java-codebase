088b5f68668d2e1c8b36761cfabbd720117969c5
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-01-08 21:47:45 +0530

    Add leap year code

---
+++ src/basics/LeapYear.java
@@ -0,0 +1,21 @@
+package basics;
+import java.util.Scanner;
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner leap = new Scanner(System.in);
+        System.out.println("Enter a year: ");
+        int year = leap.nextInt();
+        if (year % 400 == 0) {
+            System.out.println("leap year");
+        } else if (year % 100 == 0) {
+            System.out.println("not a leap year");
+        } else if (year % 4 == 0) {
+            System.out.println("leap year");
+        }
+            else {
+            System.out.println("not a leap year1900");
+            }
+    }
+}
+
+
