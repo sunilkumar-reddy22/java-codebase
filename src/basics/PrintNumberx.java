380975b26d478c30e5c9467cb0796cacb8e2f2a6
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-01-26 23:19:05 +0530

    Adding integers shuffled

--- src/basics/PrintNumberx.java
+++ src/basics/PrintNumberx.java
@@ -2,12 +2,20 @@ package basics;
 import java.util.Scanner;
 public class PrintNumberx {
     public static void main(String[] args) {
-        System.out.println("Enter a number:" );
-        Scanner in  = new Scanner(System.in);
-    //    System.out.println(in);
-           int num = in.nextInt();
-           for(int i = 1; i <= num; i++) {
-          System.out.print(i + " ");
-      }
+        System.out.println("Enter a number:");
+        Scanner in = new Scanner(System.in);
+        //    System.out.println(in);
+        int num = in.nextInt();
+        for (int i = 1; i <= num; i++) {
+            System.out.print(i + " ");
+
+            int count = 1;
+            while (count <= 5) {
+             //   System.out.print(count + " ");
+                count++;
+                int result = (i + count);
+                System.out.println(result);
+            }
+        }
     }
 }
