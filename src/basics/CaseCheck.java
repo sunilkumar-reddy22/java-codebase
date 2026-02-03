d5b6a7ea78d606935e0bfcedba5d736c09c73eea
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-02-03 22:36:11 +0530

    add case check

---
+++ src/basics/CaseCheck.java
@@ -0,0 +1,11 @@
+package basics;
+import java.util.Scanner;
+public class CaseCheck {
+    public static void main(String[] args) {
+        System.out.println("enter a char: ");
+        Scanner letter  = new Scanner(System.in);
+        char ch = letter.next().trim().charAt(0);
+        System.out.println(ch);
+
+    }
+}
